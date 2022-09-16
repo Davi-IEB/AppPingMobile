646f59832201353a2e73e4ec6f2a45235ce70d4a
davi.bento <davi.bento@itaesbra.com.br>
2022-09-16 16:07:50 -0300

    Adicionando arquivo maquinas

--- AppPingMobile/Modelo/Empresas.cs
+++ AppPingMobile/Modelo/Empresas.cs
@@ -13,4 +13,12 @@ namespace AppPingMobile.Modelo
             set { _empresa = value; }
         }
     }
+    public class Centros
+    {
+        public string Cod_empresa { get; set; }
+        public string Den_reduz { get; set; }
+        public string Setor { get; set; }
+        public string Cod_cent_trab { get; set; }
+        public string Den_cent_trab { get; set; }
+    }
 }
